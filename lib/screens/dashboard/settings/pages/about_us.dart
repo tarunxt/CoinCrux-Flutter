c1d1d5f560916dab094707fff9025cb3c942158d
Siddharthaaa21 <siddharthaarora2131@gmail.com>
2024-01-03 21:21:01 +0530

    added the divider

--- lib/screens/dashboard/settings/pages/about_us.dart
+++ lib/screens/dashboard/settings/pages/about_us.dart
@@ -9,32 +9,48 @@ class AboutUs extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: R.colors.bgColor,
-      appBar: AppBar(
-        iconTheme: IconThemeData(
-          color: R.colors.blackColor, //change your color here
-        ),
-        elevation: 0.0,
-        backgroundColor: R.colors.bgColor,
-        centerTitle: true,
-        title: Text(
-          "About Us",
-          style: R.textStyle
-              .mediumLato()
-              .copyWith(fontSize: FetchPixels.getPixelHeight(17)),
-        ),
+  backgroundColor: R.colors.bgColor,
+  appBar: AppBar(
+    iconTheme: IconThemeData(
+      color: R.colors.blackColor, // Change your color here
+    ),
+    elevation: 0.0,
+    backgroundColor: R.colors.bgColor,
+    centerTitle: true,
+    title: Text(
+      "About Us",
+      style: R.textStyle.mediumLato().copyWith(
+        fontSize: FetchPixels.getPixelHeight(17),
       ),
-      body: getPaddingWidget(
-        EdgeInsets.symmetric(horizontal: FetchPixels.getPixelWidth(20)),
-        ListView(children: [
-          getVerSpace(FetchPixels.getPixelHeight(10)),
-          Text(R.dummyData.longerText,style: R.textStyle.regularLato(),),
-          getVerSpace(FetchPixels.getPixelHeight(10)),
-          Text(R.dummyData.shortText,style: R.textStyle.regularLato(),),Text(R.dummyData.longerText,style: R.textStyle.regularLato(),),
-          getVerSpace(FetchPixels.getPixelHeight(10)),
-          Text(R.dummyData.shortText,style: R.textStyle.regularLato(),),
-        ]),
+    ),
+  ),
+  body: Padding(
+    padding: EdgeInsets.symmetric(horizontal: FetchPixels.getPixelWidth(20)),
+    child: ListView.separated(
+      itemCount: 4, // Number of items in your list
+      separatorBuilder: (BuildContext context, int index) => Divider(
+        color: Colors.grey, // Set the color of the line
+        thickness: 1, // Set the thickness of the line
+        endIndent: 16, // Adjust the end indent if needed
       ),
-    );
+      itemBuilder: (BuildContext context, int index) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            getVerSpace(FetchPixels.getPixelHeight(10)),
+            Text(
+              index.isEven
+                  ? R.dummyData.longerText
+                  : R.dummyData.shortText,
+              style: R.textStyle.regularLato(),
+            ),
+          ],
+        );
+      },
+    ),
+  ),
+);
+
+
   }
-}
+} 
