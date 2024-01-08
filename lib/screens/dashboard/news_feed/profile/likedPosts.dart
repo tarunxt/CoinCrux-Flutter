f9886160b53642591e649775873142976717fd94
Siddharthaaa21 <siddharthaarora2131@gmail.com>
2024-01-09 00:24:35 +0530

    Added and changed some liked post funtionality

--- lib/screens/dashboard/news_feed/profile/likedPosts.dart
+++ lib/screens/dashboard/news_feed/profile/likedPosts.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:coincrux/base/resizer/fetch_pixels.dart';
+import 'package:coincrux/resources/resources.dart';
 import 'package:coincrux/screens/auth/provider/auth_provider.dart';
+import 'package:coincrux/screens/dashboard/home/widgets/latest_news_widget.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -9,52 +11,64 @@ import '../model/news_model.dart';
 import '../pages/feed_view.dart';
 
 class LikedPosts extends StatelessWidget {
-  LikedPosts({super.key});
-
-  FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
-  FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  LikedPosts({Key? key});
 
   @override
   Widget build(BuildContext context) {
     FetchPixels(context);
-    AuthProviderApp authProvider = Provider.of(context, listen: false);
-    return Scaffold(
+    FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+    FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
+    // AuthProviderApp authProvider = Provider.of<AuthProviderApp>(context, listen: false);
+
+    
+       return Scaffold(
+      backgroundColor: R.colors.bgColor,
       appBar: AppBar(
+        iconTheme: IconThemeData(
+          color: R.colors.blackColor, //change your color here
+        ),
         elevation: 0.0,
-        backgroundColor: Colors.white,
-        automaticallyImplyLeading: false,
+        backgroundColor: R.colors.bgColor,
+        centerTitle: true,
         title: Text(
-          'Liked Posts',
-          style: TextStyle(fontFamily: 'Lato', color: Colors.black),
+          "Liked Posts",
+          style: R.textStyle
+              .mediumLato()
+              .copyWith(fontSize: FetchPixels.getPixelHeight(20)),
         ),
       ),
-      body: SizedBox(
-        height: FetchPixels.height,
-        width: FetchPixels.width,
-        child: StreamBuilder(
-            stream: firebaseFirestore.collection('News').snapshots(),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                List<NewsModel> news = snapshot.data!.docs
-                    .map((e) =>
-                        NewsModel.fromJson(e.data() as Map<String, dynamic>))
-                    .toList();
-                List<NewsModel> likedNews = news
-                    .where((element) => element.totalLikes!
-                        .contains(firebaseAuth.currentUser!.uid))
-                    .toList();
-                return ListView.builder(
-                  itemCount: likedNews.length,
-                  itemBuilder: (context, index) {
-                    // return FeedView(news: likedNews[index],index: index,);
-                  },
+      body: StreamBuilder(
+        stream: firebaseFirestore.collection('News').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            QuerySnapshot<Map<String, dynamic>> querySnapshot = snapshot.data as QuerySnapshot<Map<String, dynamic>>;
+            List<NewsModel> news = querySnapshot.docs
+                .map((doc) => NewsModel.fromJson(doc.data()))
+                .toList();
+            List<NewsModel> likedNews = news
+                .where((element) => element.totalLikes != null && element.totalLikes!.contains(firebaseAuth.currentUser!.uid))
+                .toList();
+
+            return ListView.builder(
+              itemCount: likedNews.length,
+              itemBuilder: (context, index) {
+                return LatestNewsWidget(
+                  news: likedNews[index],
+                  index: index,
                 );
-              } else {
-                return Center(
-                  child: SingleChildScrollView(),
-                );
-              }
-            }),
+              },
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('Error fetching data'),
+            );
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
       ),
     );
   }
